2424139fa93474b2356304b9e6dafed475b85bb5
dannyyu92 <dannyyu92@server.fake>
2013-09-19 20:46:52 -0400

    Remove info from static_pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,6 @@ class StaticPagesController < ApplicationController
   def home
   end
 
-  def info
-  end
-
   def about
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 PersonalSite::Application.routes.draw do
   get "static_pages/home"
 
-  get "static_pages/info"
-
   get "static_pages/about"
 
   get "static_pages/contact"
