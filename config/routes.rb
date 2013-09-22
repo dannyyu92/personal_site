0e63e836c74235924de1c1083a5f07e98618f264
dannyyu92 <dannyyu92@server.fake>
2013-09-22 17:31:06 -0400

    Fix routes error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 PersonalSite::Application.routes.draw do
-  root to: "static_page#home"
+  root to: "static_pages#home"
 
   match "/about", to: "static_pages#about"
   match "/contact", to: "static_pages#contact"
