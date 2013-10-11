23e290c9e75f8090669cb005b5abcc7ed6a2695e
dannyyu92 <dannyyu92@server.fake>
2013-10-11 02:24:20 -0400

    Fix migrations

--- db/migrate/20131011052120_remove_link_from_portfolio_items.rb
+++
@@ -1,9 +0,0 @@
-class RemoveLinkFromPortfolioItems < ActiveRecord::Migration
-  def up
-    remove_column :portfolio_items, :link
-  end
-
-  def down
-    add_column :portfolio_items, :link, :string
-  end
-end
--- db/migrate/20131011052248_add_site_link_to_portfolio_items.rb
+++ db/migrate/20131011052248_add_site_link_to_portfolio_items.rb
@@ -1,5 +1,6 @@
 class AddSiteLinkToPortfolioItems < ActiveRecord::Migration
   def change
     add_column :portfolio_items, :site_link, :string
+    add_column :portoflio_items, :github, :string
   end
 end
