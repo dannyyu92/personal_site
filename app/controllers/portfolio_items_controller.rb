3bd7f570f75a74f35de4dce85f75091ecf392051
dannyyu92 <dannyyu92@server.fake>
2013-11-02 17:44:44 -0400

    Tidy up articles/portfolio_items controller

--- app/controllers/portfolio_items_controller.rb
+++ app/controllers/portfolio_items_controller.rb
@@ -1,36 +1,18 @@
 class PortfolioItemsController < ApplicationController
   before_filter :authenticate_admin!, except: [:index, :show]
   # GET /portfolio_items
-  # GET /portfolio_items.json
   def index
     @portfolio_items = PortfolioItem.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @portfolio_items }
-    end
   end
 
   # GET /portfolio_items/1
-  # GET /portfolio_items/1.json
   def show
     @portfolio_item = PortfolioItem.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @portfolio_item }
-    end
   end
 
   # GET /portfolio_items/new
-  # GET /portfolio_items/new.json
   def new
     @portfolio_item = PortfolioItem.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @portfolio_item }
-    end
   end
 
   # GET /portfolio_items/1/edit
@@ -39,46 +21,32 @@ class PortfolioItemsController < ApplicationController
   end
 
   # POST /portfolio_items
-  # POST /portfolio_items.json
   def create
     @portfolio_item = PortfolioItem.new(params[:portfolio_item])
 
-    respond_to do |format|
-      if @portfolio_item.save
-        format.html { redirect_to @portfolio_item, notice: 'Portfolio item was successfully created.' }
-        format.json { render json: @portfolio_item, status: :created, location: @portfolio_item }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
-      end
+    if @portfolio_item.save
+      redirect_to @portfolio_item, notice: 'Portfolio item was successfully created.'
+    else
+      render action: "new"
     end
   end
 
   # PUT /portfolio_items/1
-  # PUT /portfolio_items/1.json
   def update
     @portfolio_item = PortfolioItem.find(params[:id])
 
-    respond_to do |format|
-      if @portfolio_item.update_attributes(params[:portfolio_item])
-        format.html { redirect_to @portfolio_item, notice: 'Portfolio item was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
-      end
+    if @portfolio_item.update_attributes(params[:portfolio_item])
+      redirect_to @portfolio_item, notice: 'Portfolio item was successfully updated.'
+    else
+      render action: "edit"
     end
   end
 
   # DELETE /portfolio_items/1
-  # DELETE /portfolio_items/1.json
   def destroy
     @portfolio_item = PortfolioItem.find(params[:id])
     @portfolio_item.destroy
 
-    respond_to do |format|
-      format.html { redirect_to portfolio_items_url }
-      format.json { head :no_content }
-    end
+    redirect_to portfolio_items_url
   end
 end
