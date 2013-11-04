022c9e536e9b4ec06882a9a559eb6e71e683c3bb
dannyyu92 <dannyyu92@server.fake>
2013-11-04 16:41:22 -0500

    Replace all tabs with two spaces for legibility

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,11 +8,11 @@ class CommentsController < ApplicationController
     redirect_to article_path(@comment.article_id)
   end
 
-	def destroy
+  def destroy
     @comment = Comment.find(params[:id])
     @comment.article_id = params[:article_id]
-		@comment.destroy
-		redirect_to article_path(@comment.article_id), :notice => "Comment deleted"
-	end
+    @comment.destroy
+    redirect_to article_path(@comment.article_id), :notice => "Comment deleted"
+  end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   attr_accessible :body, :published_at, :title, :article_image, 
-  								:article_image_cache, :remove_article_image
+                  :article_image_cache, :remove_article_image
   mount_uploader :article_image, PImageUploader
 
   has_many :comments, :dependent => :destroy
